369a780af799e8089fd5fe3ba8cccd222a677cce
Raxuis <raclot.raphael@gmail.com>
2025-01-13 15:18:28 +0100

    feat(Button): add props to allow component to get button html attributes

--- src/Button/index.tsx
+++ src/Button/index.tsx
@@ -1,20 +1,23 @@
-import {cn} from "../libs/utils.ts";
-import {ReactNode} from "react";
+import {cn} from "../libs/cn.ts";
+import {ButtonHTMLAttributes, ReactNode} from "react";
 
 type ButtonProps = {
     children: ReactNode;
     className?: string;
     onClick?: () => void;
-}
+    type?: 'submit' | 'button' | 'reset';
+} & Omit<ButtonHTMLAttributes<HTMLButtonElement>, 'className' | 'onClick' | 'type'>;
 
-const Button = ({children, className, onClick}: ButtonProps) => {
+const Button = ({children, className, onClick, type = 'button', ...props}: ButtonProps) => {
     return (
-        <div
+        <button
+            type={type}
             className={cn('block bg-medium-gray rounded-lg shadow-2xl shadow-primary/10 p-2 px-4', className)}
             onClick={onClick}
+            {...props}
         >
             <p className="font-bold">{children}</p>
-        </div>
+        </button>
     )
 };
 
