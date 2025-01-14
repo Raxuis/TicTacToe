98445980cb8d9c5ff7129c60337699b176832d21
Raxuis <raclot.raphael@gmail.com>
2025-01-14 15:30:58 +0100

    feat(reset button): add redirection when resetting

--- src/components/Board/HeaderBoard/ResetButton/index.tsx
+++ src/components/Board/HeaderBoard/ResetButton/index.tsx
@@ -2,11 +2,16 @@ import {RotateCcw} from "lucide-react";
 import Button from "../../../Button";
 import {useContext} from "react";
 import {BoardContext} from "../../../../contexts/BoardContext.tsx";
+import {useNavigate} from "react-router";
 
 const ResetButton = () => {
     const {giveUpGame} = useContext(BoardContext);
+    const navigate = useNavigate();
     return (
-        <Button onClick={() => giveUpGame()}
+        <Button onClick={() => {
+            giveUpGame()
+            navigate("/");
+        }}
                 className="bg-primary text-gray-dark shadow-buttonGreyLight active:shadow-none active:translate-y-1">
             <RotateCcw/>
         </Button>
