06a85f074a45cb9dbb0a226fe56adaf3fc842525
Raxuis <raclot.raphael@gmail.com>
2025-01-16 10:39:50 +0100

    feat(error404): add error 404 page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import {Route, Routes} from "react-router";
 import Homepage from "./pages/Homepage.tsx";
 import Layout from "./Layout";
 import TicTacToe from "./pages/TicTacToe.tsx";
+import Scoreboard from "@/pages/Scoreboard.tsx";
+import NotFound from "@/pages/NotFound.tsx";
 
 function App() {
     return (
@@ -9,6 +11,8 @@ function App() {
             <Routes>
                 <Route path="/" element={<Homepage/>}/>
                 <Route path="/tic-tac-toe" element={<TicTacToe/>}/>
+                <Route path="/scoreboard" element={<Scoreboard/>}/>
+                <Route path={"*"} element={<NotFound/>}/>
             </Routes>
         </Layout>
     )
