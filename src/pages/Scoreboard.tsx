eae923baa0ce4cb59d1f91b0a37a8a07c42d8264
Raxuis <raclot.raphael@gmail.com>
2025-01-18 10:22:21 +0100

    feat(Scoreboard): add context + provider to simplify handling

--- src/pages/Scoreboard.tsx
+++ src/pages/Scoreboard.tsx
@@ -1,40 +1,10 @@
-import {useLocalStorage} from "@/hooks/useLocalStorage";
-import {ScoreboardType, ScoreboardTypeWithRank} from "@/types";
 import ScoreboardTable from "@/components/ScoreboardTable";
-import {useState} from "react";
 import FilterScoreboard from "@/components/ScoreboardTable/FilterScoreboard";
+import {useScoreboard} from "@/hooks/useScoreboard.tsx";
 
 const Scoreboard = () => {
-    const [scoreboard] = useLocalStorage<ScoreboardType[]>("scoreboard", []);
-    const [filter, setFilter] = useState<"WIN_STREAK" | "DATE">("DATE");
+    const {scoreboard, filteredScoreboard} = useScoreboard();
 
-    const sortedScoreboardByWinStreak: ScoreboardType[] = [...scoreboard].sort(
-        (a, b) => b.winStreak - a.winStreak
-    );
-
-    const scoreboardWithRank: ScoreboardTypeWithRank[] = sortedScoreboardByWinStreak.reduce(
-        (acc: ScoreboardTypeWithRank[], curr: ScoreboardType, idx: number) => {
-            if (idx === 0) {
-                return [...acc, {...curr, rank: 1}];
-            }
-
-            const prevScore = acc[idx - 1];
-            const rank =
-                curr.winStreak === prevScore.winStreak ? prevScore.rank : idx + 1;
-
-            return [...acc, {...curr, rank}];
-        },
-        []
-    );
-
-    const sortedScoreboardByTimestamp = [...scoreboardWithRank].sort(
-        (a, b) => b.timestamp - a.timestamp
-    );
-
-    const filteredScoreboard =
-        filter === "WIN_STREAK"
-            ? scoreboardWithRank
-            : sortedScoreboardByTimestamp;
 
     return (
         <div className="overflow-x-auto">
@@ -43,7 +13,7 @@ const Scoreboard = () => {
             ) : (
                 <>
                     <ScoreboardTable scoreboard={filteredScoreboard}/>
-                    <FilterScoreboard setFilter={setFilter}/>
+                    <FilterScoreboard/>
                 </>
             )}
         </div>
---
+++ src/providers/ScoreboardProvider.tsx
@@ -0,0 +1,51 @@
+import {ReactNode, useState} from "react";
+import {useLocalStorage} from "@/hooks/useLocalStorage.ts";
+import {ScoreboardType, ScoreboardTypeWithRank} from "@/types";
+import {ScoreboardContext} from "@/contexts/ScoreboardContext.tsx";
+
+export const ScoreboardProvider = ({children}: { children: ReactNode }) => {
+    const [scoreboard] = useLocalStorage<ScoreboardType[]>("scoreboard", []);
+    const [filter, setFilter] = useState<"WIN_STREAK" | "DATE">("DATE");
+
+    const sortedScoreboardByWinStreak: ScoreboardType[] = [...scoreboard].sort(
+        (a, b) => b.winStreak - a.winStreak
+    );
+
+    const scoreboardWithRank: ScoreboardTypeWithRank[] = sortedScoreboardByWinStreak.reduce(
+        (acc: ScoreboardTypeWithRank[], curr: ScoreboardType, idx: number) => {
+            if (idx === 0) {
+                return [...acc, {...curr, rank: 1}];
+            }
+
+            const prevScore = acc[idx - 1];
+            const rank =
+                curr.winStreak === prevScore.winStreak ? prevScore.rank : idx + 1;
+
+            return [...acc, {...curr, rank}];
+        },
+        []
+    );
+
+    const sortedScoreboardByTimestamp = [...scoreboardWithRank].sort(
+        (a, b) => b.timestamp - a.timestamp
+    );
+
+
+    const filteredScoreboard =
+        filter === "WIN_STREAK"
+            ? scoreboardWithRank
+            : sortedScoreboardByTimestamp;
+
+    const contextValue = {
+        scoreboard,
+        filteredScoreboard,
+        filter,
+        setFilter,
+    };
+
+    return (
+        <ScoreboardContext.Provider value={contextValue}>
+            {children}
+        </ScoreboardContext.Provider>
+    );
+}
